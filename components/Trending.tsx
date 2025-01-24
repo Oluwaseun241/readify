8c3cb61874c39489c9f39f2525cbb924e55db282
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2025-01-24 12:04:56 +0100

    wip: home image

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,14 +1,32 @@
-import { Pressable, Text, View } from "react-native";
+import { Data } from "@/constants/data";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
+
+const Item = ({ image }: any) => {
+  return (
+    <View className="my-4 ml-5">
+      <Image source={image} resizeMode="contain" className="w-24 h-36" />
+    </View>
+  );
+};
 
 const Trending = () => {
   return (
-    <View className="flex flex-row justify-between my-4 mx-5">
-      <Text className="text-xl font-[ArimaBold]">Trending</Text>
-      <Pressable>
-        <Text className="font-[ArimaMedium] text-primary-textTint">
-          See all
-        </Text>
-      </Pressable>
+    <View>
+      <View className="flex flex-row justify-between my-4 mx-5">
+        <Text className="text-xl font-[ArimaBold]">Trending</Text>
+        <Pressable>
+          <Text className="font-[ArimaMedium] text-primary-textTint">
+            See all
+          </Text>
+        </Pressable>
+      </View>
+      <FlatList
+        data={Data}
+        renderItem={({ item }) => <Item image={item.image} />}
+        keyExtractor={(item) => item.id}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+      />
     </View>
   );
 };
---
+++ constants/data.ts
@@ -0,0 +1,18 @@
+export const Data = [
+  {
+    id: "1",
+    image: require("@/assets/images/book1.png"),
+  },
+  {
+    id: "2",
+    image: require("@/assets/images/book2.png"),
+  },
+  {
+    id: "3",
+    image: require("@/assets/images/book3.png"),
+  },
+  {
+    id: "4",
+    image: require("@/assets/images/book4.png"),
+  },
+];
